68f99bf4127bfe3b0fb10f5e9f360d7b210e5502
VirtoOZ <109903427+VirtoOZ@users.noreply.github.com>
2024-07-23 07:29:26 +0300

    mainblock adapt

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,13 +1,13 @@
 //<BURGER>=================================
-// const iconMenu = document.querySelector('.menu__icon');//находим класс menu__icon
-// const menuBody = document.querySelector('.menu__body');//находим класс menu__body
-// if (iconMenu) {//Проверяем есть ли menu__icon
-// 	iconMenu.addEventListener("click", function (e) {//вещам событие при клике мыши
-// 		document.body.classList.toggle('_lock');//Для Body даем класс Lock для отключения прокрутки
-// 		iconMenu.classList.toggle('_active');//добавляем класс active menu__icon
-// 		menuBody.classList.toggle('_active');//добавляем класс active menu__body
-// 	});
-// };
+const iconMenu = document.querySelector('.menu__icon');//находим класс menu__icon
+const menuBody = document.querySelector('.menu__body');//находим класс menu__body
+if (iconMenu) {//Проверяем есть ли menu__icon
+	iconMenu.addEventListener("click", function (e) {//вещам событие при клике мыши
+		document.body.classList.toggle('_lock');//Для Body даем класс Lock для отключения прокрутки
+		iconMenu.classList.toggle('_active');//добавляем класс active menu__icon
+		menuBody.classList.toggle('_active');//добавляем класс active menu__body
+	});
+};
 //</BURGER>=================================
 
 //<BURGER SIDE-MENU>=================================
